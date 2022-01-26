9dbc4a293dd8252fd3fe3edc103880fa8ac9cbb4
IVAN <Sevkooo89@gmail.com>
2022-01-26 19:01:17 +0300

    поправил

--- src/lesson_01_homework/HomeWork_1.java
+++ src/lesson_01_homework/HomeWork_1.java
@@ -56,8 +56,8 @@ public class HomeWork_1 {
     public static void printColor(int value) {
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (value <= 0 && value <= 100) {
-            System.out.println("Красный");
+        } else if (value >= 0 && value <= 100) {
+            System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
         }
